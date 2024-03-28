b2f9a69103863573a0c10d05c922e485a1c9df35
Lê Thái Sơn <lethaison04011999@gmail.com>
2024-03-28 17:13:48 +0700

    init code base

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
 const app = express()
+
 const hostname = 'localhost'
 const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('Hello Worlds!')
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-  console.log(` App is running on port: https://${hostname}:${port}`)
+  // eslint-disable-next-line no-console
+  console.log(`Hello Thai Son Dev, Server is running on https://${hostname}:${port}`)
 })
---
+++ src/utils/algorithms.js
@@ -0,0 +1,1 @@
+// Thai son dev
