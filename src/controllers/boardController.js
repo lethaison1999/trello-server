dbe80bddc7f9c2b945c69c36d00ee554f78980d9
Lê Thái Sơn <lethaison04011999@gmail.com>
2024-04-03 15:49:00 +0700

    error handling midleware

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes'
-
+// import ApiError from '~/utils/ApiError'
 /* 
-- route->validation->controller->services->model
+- route->validation->controller->(học qua error Handling) ->services->model
 - controller điều hướng sang services 
 - req.query: v1/board?author=thaisondev&height=170 , querystring, tìm kiếm => ?
 - req.params: v1/board/:id => v1/board/123 => id=123
@@ -9,14 +9,16 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
+    // console.log(req.body)
+    // res.status(StatusCodes.CREATED).json({
+    //   message: 'POST from validation api create broad'
+    // })
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, 'thaisondev test error')
     res.status(StatusCodes.CREATED).json({
       message: 'POST from validation api create broad'
     })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
+    next(error)
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import exitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
+
 const START_SERVER = () => {
   const app = express()
 
@@ -12,6 +14,9 @@ const START_SERVER = () => {
 
   //use APIs v1
   app.use('/v1', APIs_V1)
+
+  //middleware xử lý lỗi chung
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
